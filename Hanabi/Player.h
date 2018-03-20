df44692d83d95bb3c1b94ec4f25bb379362de3ce
Samuel Kenney <samuel.kenney48@gmail.com>
2018-03-19 23:30:40 -0400

    After some testing, tell seems to be pretty stable and updates the knowledge base accordingly

--- Hanabi/Player.h
+++ Hanabi/Player.h
@@ -6,6 +6,8 @@
 #include <map>
 #include <algorithm>
 #include <list>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 using std::vector;
 using std::cout;
@@ -24,11 +26,13 @@ public:
 	Event* ask();
 protected:
 	// players hand that holds every possible card
-	std::map<int, std::list<int>> playerHand;
+	std::map<int, std::map<int, std::list<int>>> playerHand;
 	// deck that holds every possible card
 	std::map<int, std::list<int>> deck;
 	// map that holds the tableu
 	std::map<int, std::list<int>> tableu;
+	// discard pile
+	std::vector<Card> discardPile;
 
 	// elements for the Knowledge base
 	int hints;
@@ -36,8 +40,6 @@ protected:
 	// easily can store other hand
 	vector<Card> oHand;
 	int deckSize;
-
-	bool first_clear;
 };
 
 Player::Player()
@@ -63,14 +65,6 @@ Player::Player()
 	possiblecards.push_back(4);
 	possiblecards.push_back(5);
 
-	// fill the player's hand with all the possible cards
-	// RED 0 BLUE 1 GREEN 2 YELLOW 3 WHITE 4
-	playerHand[0] = possiblecards; //Position 0
-	playerHand[1] = possiblecards; //Position 1
-	playerHand[2] = possiblecards; //Position 2
-	playerHand[3] = possiblecards; //Position 3
-	playerHand[4] = possiblecards; //Position 4
-
 	// fill the deck with every possible card
 	deck[0] = possiblecards; //RED
 	deck[1] = possiblecards; //BLUE
@@ -78,7 +72,20 @@ Player::Player()
 	deck[3] = possiblecards; //YELLOW
 	deck[4] = possiblecards; //WHITE
 
-	first_clear = false;
+	// fill the player's hand with all the possible cards
+	// RED 0 BLUE 1 GREEN 2 YELLOW 3 WHITE 4
+	playerHand[0] = deck; //Position 0
+	playerHand[1] = deck; //Position 1
+	playerHand[2] = deck; //Position 2
+	playerHand[3] = deck; //Position 3
+	playerHand[4] = deck; //Position 4
+
+	std::list<int> empty;
+	tableu[0] = empty;
+	tableu[1] = empty;
+	tableu[2] = empty;
+	tableu[3] = empty;
+	tableu[4] = empty;
 }
 
 Player::Player(const Player& p)
@@ -91,23 +98,16 @@ Player::Player(const Player& p)
 	playerHand = p.playerHand;
 	deck = p.deck;
 	tableu = p.tableu;
+	discardPile = p.discardPile;
 }
 
 void Player::tell(Event* e, vector<int> board, int hints, int fuses, vector<Card> oHand, int deckSize)
 {
-	// This function will store the events and their results into the knowledge base
-	
-	// go through deck and remove each instance of possible card from the deck from other Player's card
-	if (oHand.size() == 5 && !first_clear){
-		for (int i = 0; i < oHand.size(); i++)
-		{
-			// Removes the card from the map that holds the deck
-			std::list<int>::iterator it = std::find(deck.at(oHand[i].color).begin(), deck.at(oHand[i].color).end(), oHand[i].number);
-			deck.at(oHand[i].color).erase(it);
-		}
-		first_clear = true; // make sure deck does not keep getting cleared
-	}
+	// ============================================================================================
+	// =======This function will store the events and their results into the knowledge base========
+	// ============================================================================================
 
+	// go through deck and remove each instance of possible card from the deck from other Player's card
 	int currentAction = e->getAction();
 	/*
 	PLAY 11
@@ -124,20 +124,63 @@ void Player::tell(Event* e, vector<int> board, int hints, int fuses, vector<Card
 			// take away from player hand or other hand
 			if (pe->wasItThisPlayer){
 				// remove all options from this position in the players hand-- will be replaced by draw
-				playerHand.at(pe->c.color).clear();
-			} else { // the other players hand needs to be updated
-				oHand.at(pe->position).color = -1; // TODO: figure out a better way than sentinel value for no card in oHand
+				playerHand.at(pe->position) = deck; // cards that are left over in the deck, not all 50 cards
 			}
 
 			// push card onto the tableu when it is a valid move
 			tableu.at(pe->c.color).push_back(pe->c.number); 
 
+			// first  red                   green                blue
+			// <0 < 0 <1 1 1 2 2 3 3 4 4 5> 1 <1 1 1 2 2 3 3 4 4 5> ... > > >
+			// need to update own hand to reflect that it is no longer an option
+			for (int i = 0; i < playerHand.size(); i++)
+			{
+				// Removes the card from the map that holds the deck
+				if (!playerHand.at(i).empty() && !playerHand.at(i).at(pe->c.color).empty()){
+					// Removes the card from the map that holds the deck
+					std::list<int>::iterator it = std::find(playerHand.at(i).at(pe->c.color).begin(), playerHand.at(i).at(pe->c.color).end(), pe->c.number);
+					if (it != playerHand.at(i).at(pe->c.color).end())
+						playerHand.at(i).at(pe->c.color).erase(it);		
+				}	
+			}
+
 			// Removes the card from the map that holds the deck
-			std::list<int>::iterator it = std::find(deck.at(pe->c.color).begin(), deck.at(pe->c.color).end(), pe->c.number);
-			deck.at(pe->c.color).erase(it);
+			std::list<int>::iterator it = std::find(deck.at(pe->c.color).begin(), deck.at(pe->c.color).end(),pe->c.number);
+			// makes sure there are no out of bound errors
+			if (it != deck.at(pe->c.color).end())
+				deck.at(pe->c.color).erase(it);
 
 		} else { // if not legal, take away a fuse
-			fuses--;
+			// don't need to worry about the game ending, this is for my knowledge base
+			// if it is not a legal move, needs to be discarded
+
+			// take away from player hand or other hand
+			if (pe->wasItThisPlayer){
+				// remove all options from this position in the players hand-- will be replaced by draw
+				playerHand.at(pe->position) = deck;
+			} 
+
+			//add discarded card to the pile
+			discardPile.push_back(pe->c);
+
+			// first  red                   green                blue
+			// <0 < 0 <1 1 1 2 2 3 3 4 4 5> 1 <1 1 1 2 2 3 3 4 4 5> ... > > >
+			// need to update own hand to reflect that it is no longer an option
+			for (int i = 0; i < playerHand.size(); i++)
+			{
+				if (!playerHand.at(i).empty() && !playerHand.at(i).at(pe->c.color).empty()){
+					// Removes the card from the map that holds the deck
+					std::list<int>::iterator it = std::find(playerHand.at(i).at(pe->c.color).begin(), playerHand.at(i).at(pe->c.color).end(), pe->c.number);
+					if (it != playerHand.at(i).at(pe->c.color).end())
+						playerHand.at(i).at(pe->c.color).erase(it);			
+				}
+			}
+
+			// Removes the card from the map that holds the deck
+			std::list<int>::iterator it = std::find(deck.at(pe->c.color).begin(), deck.at(pe->c.color).end(), pe->c.number);
+			// makes sure there are no out of bound errors
+			if (it != deck.at(pe->c.color).end())
+				deck.at(pe->c.color).erase(it);
 		}
 	}
 	else if(currentAction == 12){
@@ -145,29 +188,85 @@ void Player::tell(Event* e, vector<int> board, int hints, int fuses, vector<Card
 		// depending on who it is, the card is removed from the hand and the deck
 		if (de->wasItThisPlayer){
 			// remove all options from this position in the players hand-- will be replaced by draw
-			playerHand.at(de->c.color).clear();
+			playerHand.at(de->position).at(de->c.color).clear();
 		} else {
 			oHand.at(de->position).color = -1; // TODO: figure out a better way than sentinel value for no card in oHand
 		}
 
+		// add discarded card to the pile
+		discardPile.push_back(de->c);
+
+		// update the players hand based on what is discarded
+		for (int i = 0; i < playerHand.size(); i++)
+		{
+			if (!playerHand.at(i).empty() && !playerHand.at(i).at(de->c.color).empty()){
+				// Removes the card from the map that holds the deck
+				std::list<int>::iterator it = std::find(playerHand.at(i).at(de->c.color).begin(), playerHand.at(i).at(de->c.color).end(), de->c.number);
+				if (it != playerHand.at(i).at(de->c.color).end())
+					playerHand.at(i).at(de->c.color).erase(it);		
+			}
+		}
+
 		// Removes the card from the map that holds the deck
-		std::list<int>::iterator it = std::find(deck.at(de->c.color).begin(), deck.at(de->c.color).end(), de->c.number);
-		deck.at(de->c.color).erase(it);
+		std::list<int>::iterator it = std::find(deck.at(de->c.number).begin(), deck.at(de->c.number).end(), de->c.color);
+		// makes sure there are no out of bound errors
+		if (it != deck.at(de->c.color).end())
+			deck.at(de->c.color).erase(it);
 	}
 	else if (currentAction == 13){
 		ColorHintEvent* ce = (ColorHintEvent*) e;
-		// TODO:
+		for (int i = 0; i < ce->indices.size(); i++)
+		{
+			// first  red                   green                blue
+			// <0 < 0 <1 1 1 2 2 3 3 4 4 5> 1 <1 1 1 2 2 3 3 4 4 5> ... > > >
+			for (int j = 0; j < playerHand.at(i).size(); j++)
+			{
+				// for each color not in hint, remove from the map
+				// grab the position of the index specified in indices at i
+				if (j != ce->color)
+					playerHand.at(ce->indices.at(i)).at(j).clear();
+			}
+		}
 	}
 	else if (currentAction == 14){
+		// will always be for the other player since we can see it
 		DrawEvent* dre = (DrawEvent*) e;
-		// TODO:
+
+		// removes drawn card from the player's hand
+		for (int i = 0; i < playerHand.size(); i++)
+		{
+			if (!playerHand.at(i).empty() && !playerHand.at(i).at(dre->drawnCard.color).empty()){
+				// Removes the card from the map that holds the deck
+				std::list<int>::iterator it = std::find(playerHand.at(i).at(dre->drawnCard.color).begin(), playerHand.at(i).at(dre->drawnCard.color).end(), dre->drawnCard.number);
+				if (it != playerHand.at(i).at(dre->drawnCard.color).end())
+					playerHand.at(i).at(dre->drawnCard.color).erase(it);		
+			}
+		}
+
+		// Removes the card from the map that holds the deck
+		std::list<int>::iterator it = std::find(deck.at(dre->drawnCard.color).begin(), deck.at(dre->drawnCard.color).end(), dre->drawnCard.number);
+		// makes sure there are no out of bound errors
+		if (it != deck.at(dre->drawnCard.color).end())
+			deck.at(dre->drawnCard.color).erase(it);
 	}
 	else if (currentAction == 15){
 		NumberHintEvent* ne = (NumberHintEvent*) e;
-		// TODO:
+
+		for (int i = 0; i < ne->indices.size(); i++)
+		{
+			// <0, 2, 4> are blue
+			// first  red                   green                blue
+			// <0 < 0 <1 1 1 2 2 3 3 4 4 5> 1 <1 1 1 2 2 3 3 4 4 5> ... > > >
+			for (int j = 0; j < playerHand.at(i).size(); j++)
+			{
+				// grab the position of the index specified in indices at i
+				playerHand.at(ne->indices.at(i)).at(j).clear();
+				playerHand.at(ne->indices.at(i)).at(j).push_back(ne->number);
+			}
+		}
 	}
 	else if (currentAction == 0){
-		// TODO:
+		// TODO: Probably nothing
 	}
 
 	this->hints = hints;
@@ -197,20 +296,73 @@ void Player::tell(Event* e, vector<int> board, int hints, int fuses, vector<Card
 
 Event* Player::ask()
 {
-	Event* foo = new Event();
-	return foo;
-	/* You must produce an event of the appropriate type. Not all member
-	variables of a given event type need to be filled in; some will be
-	ignored even if they are. Summary follows.
-	Options:
-	ColorHintEvent - you must declare a color; no other member variables
-	necessary.
-	NumberHintEvent - you must declare a number; no other member variables
-	necessary.
-	PlayEvent - you must declare the index to be played; no other member
-	variables necessary.
-	DiscardEvent - you must declare the index to be discarded; no other
-	member variables necessary.
+	/*
+	std::vector<int> foo;
+	foo.push_back(0);
+	foo.push_back(1);
+	foo.push_back(2);
+	ColorHintEvent* color = new ColorHintEvent(foo, 0); //should remove all non-red cards from first, second, and third, card
+	*/
+	//Event* foo = new Event();
+	//return foo;
+
+	srand (time(NULL));
+
+	int choice = rand()%100;
+
+	if (choice > 50){
+		std::vector<int> foo;
+		foo.push_back(0);
+		foo.push_back(1);
+		foo.push_back(2);
+		foo.push_back(3);
+		foo.push_back(4);
+		NumberHintEvent* number = new NumberHintEvent(foo, 2); //should remove all non-red cards from first, second, and third, card
+		return number;
+	} else {
+
+		PlayEvent* play = new PlayEvent(0);
+		return play;
+	}
+	/*
+	 What do I have in my knowledge base to make a decision?
+		- Player's Hand: or what could be
+		- Deck: Cards left in the deck
+		- Tableu: cards played to win the game
+		- Discard Pile: cards that have been discarded or poorly played
+
+		-// players hand that holds every possible card
+	std::map<int, std::map<int, std::list<int>>> playerHand;
+	// deck that holds every possible card
+	std::map<int, std::list<int>> deck;
+	// map that holds the tableu
+	std::map<int, std::list<int>> tableu;
+	// discard pile
+	std::vector<Card> discardPile;
+
+	// elements for the Knowledge base
+	int hints;
+	int fuses;
+	// easily can store other hand
+	vector<Card> oHand;
+	int deckSize;
+
+	*/
+
+
+
+	/*  You must produce an event of the appropriate type. Not all member
+		variables of a given event type need to be filled in; some will be
+		ignored even if they are. Summary follows.
+		Options:
+			ColorHintEvent - you must declare a color; no other member variables
+				necessary.
+			NumberHintEvent - you must declare a number; no other member variables
+				necessary.
+			PlayEvent - you must declare the index to be played; no other member
+				variables necessary.
+			DiscardEvent - you must declare the index to be discarded; no other
+				member variables necessary.
 	*/
 }
 #endif
